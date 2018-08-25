5bd3e1a291cc2d31a625856080d3bf666d1387e2
Victor Melo <victor.contas@outlook.com>
2018-08-25 17:28:42 -0300

    All setups for developing persistence flow were defined

--- WhatsNewInARKit2/WhatsNewInARKit2/Application/Map Persistence/MapPersistenceCoordinator.swift
+++ WhatsNewInARKit2/WhatsNewInARKit2/Application/Map Persistence/MapPersistenceCoordinator.swift
@@ -2,11 +2,17 @@ import UIKit
 
 class MapPersistenceCoordinator: Coordinator {
     private var presenter: UINavigationController
-    private var mapPersistenceViewController: MapPersistenceViewController
+    private var mapPersistenceViewController: MapPersistenceViewController?
     
-    
+    init(presenter: UINavigationController) {
+        self.presenter = presenter
+    }
     
     func start() {
+        let mapPersistenceViewController = MapPersistenceViewController(nibName: nil, bundle: nil)
+        mapPersistenceViewController.title = "Persistence Demo"
+        presenter.pushViewController(mapPersistenceViewController, animated: true)
         
+        self.mapPersistenceViewController = mapPersistenceViewController
     }
 }
