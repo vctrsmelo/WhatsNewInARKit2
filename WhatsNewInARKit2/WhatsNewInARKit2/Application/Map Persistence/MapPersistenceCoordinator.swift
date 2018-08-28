26825a1195cd6ed4ad4d8509af5ea34d73b54949
Victor Melo <victor.contas@outlook.com>
2018-08-28 13:21:57 -0300

    Removed unused code

--- WhatsNewInARKit2/WhatsNewInARKit2/Application/Map Persistence/MapPersistenceCoordinator.swift
+++ WhatsNewInARKit2/WhatsNewInARKit2/Application/Map Persistence/MapPersistenceCoordinator.swift
@@ -3,7 +3,6 @@ import UIKit
 class MapPersistenceCoordinator: Coordinator {
     private var presenter: UINavigationController
     private var persistingViewController: PersistingMapViewController?
-    private var loadingMapViewController: LoadingMapViewController?
     
     init(presenter: UINavigationController) {
         self.presenter = presenter
@@ -12,19 +11,8 @@ class MapPersistenceCoordinator: Coordinator {
     func start() {
         let persistingViewController = PersistingMapViewController(nibName: nil, bundle: nil)
         persistingViewController.title = "Persistence Demo"
-//        persistingViewController.navigationItem.setRightBarButton(UIBarButtonItem(title: "Load Demo", style: .plain, target: self, action: #selector(presentLoadingMapViewController))
-//            , animated: false)
-        
         presenter.pushViewController(persistingViewController, animated: true)
         
         self.persistingViewController = persistingViewController
     }
-    
-////    @objc
-////    func presentLoadingMapViewController(sender: UIBarButtonItem) {
-////        let loadingMapViewController = LoadingMapViewController(nibName: nil, bundle: nil)
-////        loadingMapViewController.title = "Loading Demo"
-////        presenter.pushViewController(loadingMapViewController, animated: true)
-////        self.loadingMapViewController = loadingMapViewController
-//    }
 }
